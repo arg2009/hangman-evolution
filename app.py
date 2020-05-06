fe8ca766731afb5ec29b045eaa0ca2a915487d02
Ruben Funai <ruben.funai@whispli.com>
2020-05-07 00:01:29 +1000

    Added basic hello-world web app

---
+++ app.py
@@ -0,0 +1,14 @@
+import web
+
+urls = (
+    '/', 'Index'
+)
+
+class Index:
+    def GET(self):
+        return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
