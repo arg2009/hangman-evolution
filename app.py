707e5bb5576508e6f7838898803d157fb9918f96
Ruben Funai <ruben.funai@whispli.com>
2020-05-07 00:48:45 +1000

    Implemented skeleton code for getting a random word

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 # Import required modules
 import web
+import json
 
 # Associate web route to handling class
 urls = (
-    '/', 'Index'
+    # View Routes
+    '/', 'Index',
+    
+    # API Routes
+    '/api/random-word', 'ApiWord',
 )
 
 # Setup the default rendering engine
@@ -14,6 +19,18 @@ default_render = web.template.render('views/', base='layout')
 class Index:
     def GET(self):
         return default_render.index()
+
+
+# /api/random-word Route
+class ApiWord:
+    def GET(self):
+        # Set content type
+        web.header("Content-Type", "application/json")
+
+        # TODO: implement this propery by retrieving a random word and hint from some sort of data store
+        data = {"word": "fun", "hint": "A noun"}
+
+        return json.dumps(data)
 
 
 # Run the web application
