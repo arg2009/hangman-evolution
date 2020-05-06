cf7701e5a60d134dc6aad06c53c163e120a7d01c
Ruben Funai <ruben.funai@whispli.com>
2020-05-07 00:33:45 +1000

    Added assistive comments

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
+# Import required modules
 import web
 
+# Associate web route to handling class
 urls = (
     '/', 'Index'
 )
 
+# Setup the default rendering engine
 default_render = web.template.render('views/', base='layout')
 
 
+# / Route
 class Index:
     def GET(self):
         return default_render.index()
 
 
+# Run the web application
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
